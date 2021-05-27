cf8a2898ae2cf9e6a976a8ee0875c1c933de6f93
Meghna Baijal <mbaijal@amazon.com>
2021-05-27 03:54:05 -0700

    Change S3 bucket sync logic based on region

--- test/e2e/service_bootstrap.py
+++ test/e2e/service_bootstrap.py
@@ -18,6 +18,8 @@ import boto3
 import logging
 from time import sleep
 import json
+import time
+import subprocess
 
 from acktest import resources
 from acktest.aws.identity import get_region, get_account_id
@@ -109,7 +111,30 @@ def create_data_bucket() -> str:
 
     source_bucket = s3_resource.Bucket(SAGEMAKER_SOURCE_DATA_BUCKET)
     destination_bucket = s3_resource.Bucket(bucket_name)
-    duplicate_bucket_contents(source_bucket, destination_bucket)
+    temp_dir = "/tmp/ack_s3_data"
+    # awscli is not installed in test-infra container hence use boto3 to copy in us-west-2
+    if region == "us-west-2":
+        duplicate_bucket_contents(source_bucket, destination_bucket)
+        # above method does an async copy
+        # TODO: find a way to remove random wait
+        sleep(180)
+    else:
+        # workaround to copy if buckets are across regions
+        # TODO: check if there is a better way and merge to test-infra
+        subprocess.call(["mkdir", f"{temp_dir}"])
+        subprocess.call(
+            [
+                "aws",
+                "s3",
+                "sync",
+                f"s3://{SAGEMAKER_SOURCE_DATA_BUCKET}",
+                f"./{temp_dir}/",
+                "--quiet",
+            ]
+        )
+        subprocess.call(
+            ["aws", "s3", "sync", f"./{temp_dir}/", f"s3://{bucket_name}", "--quiet"]
+        )
 
     logging.info(f"Synced data bucket")
 
