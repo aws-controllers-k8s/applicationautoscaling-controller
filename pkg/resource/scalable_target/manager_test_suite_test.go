012cd7b262c28fc610e5f99a8a968124e6df0a32
Steakley <ryanstea@amazon.com>
2021-11-11 10:24:07 -0800

    Remove duplicate unit test work aswell as methods using shell commands

--- pkg/resource/scalable_target/manager_test_suite_test.go
+++ pkg/resource/scalable_target/manager_test_suite_test.go
@@ -17,6 +17,9 @@ import (
 	"errors"
 	"fmt"
 
+	"path/filepath"
+	"testing"
+
 	svcapitypes "github.com/aws-controllers-k8s/applicationautoscaling-controller/apis/v1alpha1"
 	"github.com/aws-controllers-k8s/applicationautoscaling-controller/pkg/testutil"
 	mocksvcsdkapi "github.com/aws-controllers-k8s/applicationautoscaling-controller/test/mocks/aws-sdk-go/applicationautoscaling"
@@ -24,13 +27,10 @@ import (
 	ackmetrics "github.com/aws-controllers-k8s/runtime/pkg/metrics"
 	acktypes "github.com/aws-controllers-k8s/runtime/pkg/types"
 	svcsdk "github.com/aws/aws-sdk-go/service/applicationautoscaling"
-	"github.com/ghodss/yaml"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"go.uber.org/zap/zapcore"
-	"path/filepath"
 	ctrlrtzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"testing"
 )
 
 // provideResourceManagerWithMockSDKAPI accepts MockApplicationAutoScalingAPI and returns pointer to resourceManager
@@ -124,13 +124,4 @@ func (d *testRunnerDelegate) Equal(a acktypes.AWSResource, b acktypes.AWSResourc
 	}
 
 	return statusMatch && specMatch
-}
-
-// Checks to see if the given yaml file, with name stored as expectation,
-// matches the yaml marshal of the AWSResource stored as actual.
-func (d *testRunnerDelegate) YamlEqual(expectation string, actual acktypes.AWSResource) bool {
-	// Build a tmp file for the actual yaml.
-	actualResource := actual.(*resource)
-	actualYamlByteArray, _ := yaml.Marshal(actualResource.ko)
-	return testutil.IsYamlEqual(&expectation, &actualYamlByteArray)
 }
