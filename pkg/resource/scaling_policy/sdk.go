a5bf58b06a20d57cc1c8712d8a87d805b820fd1c
ci-robot <ci-robot@k8s.io>
2023-03-07 21:40:01 +0000

    Update to ACK runtime `v0.24.1`, code-generator `v0.24.3`

--- pkg/resource/scaling_policy/sdk.go
+++ pkg/resource/scaling_policy/sdk.go
@@ -460,7 +460,7 @@ func (rm *resourceManager) sdkUpdate(
 	defer func() {
 		exit(err)
 	}()
-	input, err := rm.newUpdateRequestPayload(ctx, desired)
+	input, err := rm.newUpdateRequestPayload(ctx, desired, delta)
 	if err != nil {
 		return nil, err
 	}
@@ -510,6 +510,7 @@ func (rm *resourceManager) sdkUpdate(
 func (rm *resourceManager) newUpdateRequestPayload(
 	ctx context.Context,
 	r *resource,
+	delta *ackcompare.Delta,
 ) (*svcsdk.PutScalingPolicyInput, error) {
 	res := &svcsdk.PutScalingPolicyInput{}
 
